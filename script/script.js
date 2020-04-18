30f1262f0804625f2f29996369d194c5676a19d1
austinvuong <austin.r.vuong@gmail.com>
2020-04-18 13:03:20 -0400

    Button shuffling

--- script/script.js
+++ script/script.js
@@ -1,3 +1,46 @@
+var buttons = [];
+
 $(document).ready(function(){
-	alert("jquery");
+
+	// append buttons
+	for (var i = 0; i < 4; i++) {
+
+		var button = document.createElement("button");
+		button.type = "button";
+		button.innerHTML = "Button #" + i;
+		
+		button.onclick = function() {
+			alert("I am " + this.innerHTML);
+		};
+		
+		$("body").append(button);
+		
+		buttons.push(button);
+	}
+	
+	shuffle();
+	
 });
+
+function shuffle() {
+	
+	var b; // button
+
+	// make everything the incorrect answer
+	for (var i = 0; i < 4; i++) {
+		b = buttons[i];
+		
+		b.innerHTML = "Wrong";
+		b.onclick = function() {
+			alert("But ... I said \"wrong\"!");
+		};
+	}
+	
+	// set one to the correct answer
+	b = buttons[Math.floor((Math.random() * 4))];
+	b.innerHTML = "CLICK ME!!!"
+	b.onclick = function() {
+		alert("Hurray!");
+		shuffle();
+	};
+}
