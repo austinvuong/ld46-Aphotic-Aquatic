75e2c36dcc1e33c687d018d414467e1ee27b4e3a
austinvuong <austin.r.vuong@gmail.com>
2020-04-18 17:17:45 -0400

    Load sample image

--- script/canvasScript.js
+++ script/canvasScript.js
@@ -1,3 +1,30 @@
-$(document).ready(function(){
+var stage;
+var Scene = {
+	NULL: 0,
+	OFFICE: 1,
 	
+	img: {
+		1: new createjs.Bitmap("res/img/sample-office.png")
+	}
 }
+
+
+$(document).ready(function(){
+	stage = new createjs.Stage("gameCanvas");
+	
+	// TEMP use a preloader. Will still do a timer somehow
+	createjs.Ticker.on("tick", tick);
+	function tick(event) {
+		stage.update(event);
+	}
+	
+	var img = Scene.img[Scene.OFFICE];
+	
+	img.x = -32;
+	img.y = -64;
+		
+	stage.addChild(img);
+	console.log(stage);
+
+	stage.update();
+});
