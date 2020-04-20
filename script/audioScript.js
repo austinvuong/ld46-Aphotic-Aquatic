7aa9843783a105d49c29da25d15152c64017b043
austinvuong <austin.r.vuong@gmail.com>
2020-04-20 17:08:03 -0400

    Clean up

--- script/audioScript.js
+++ script/audioScript.js
@@ -1,25 +1,3 @@
-const PATH_AUDIO = "res/audio/"
-
-let Audio = {
-	NULL: 0,
-	OFFICE: 1,
-	GOLDIE: 2,
-	ANGEL: 3,
-	SEAHORSE: 4,
-  JELLY: 5,
-  BETTA: 6,
-	
-	path: {
-		0: PATH + "testImg.png",
-		1: PATH + "room.png",
-		2: PATH + "goldie.gif",
-    3: PATH + "angel.gif",
-		4: PATH + "seahorse.gif",
-		5: PATH + "jellies.gif",
-    6: PATH + "betta.gif",
-	}
-}
-
 function play() {
   $("#ambient")[0].play();
 }
